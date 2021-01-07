445b54e1080c3d5ab5967487b5c464ed46c81255
nyosru <nyos@rambler.ru>
2021-01-07 23:58:41 +0500

    0.1.0

--- README.md
+++
@@ -1,2 +0,0 @@
-# composer_didrive_api_1c
-работа с 1с
---
+++ src/class.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+  класс модуля
+ * */
+
+namespace Nyos\api;
+
+//ini_set("max_execution_time", 120);
+
+/**
+ * работа с 1С
+ */
+class API_1C {
+
+    public static $host = null;
+
+    public static function start() {
+        
+    }
+
+}
